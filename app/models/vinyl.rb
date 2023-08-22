1c59218dc7f11f25646a30a23a10bc6a78d5e0f2
Anastasia Florentis <anastasiaflorentis@gmail.com>
2023-08-22 18:00:30 +0100

    seeded and worked on the index page

--- app/models/vinyl.rb
+++ app/models/vinyl.rb
@@ -2,9 +2,10 @@ class Vinyl < ApplicationRecord
   belongs_to :user
   #has_one_attached :cover_image
   has_many :bookings
+
   validates :album_title, presence: true
-  validates :cover_image, presence: true
-  validates :release_year, presence: true
+  # validates :cover_image, presence: true
+  # validates :release_year, presence: true
   validates :genre, presence: true
   validates :songs, presence: true
 end
