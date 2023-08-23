8be44349a495d679d6ca6b5a9cc3b7cb9f40593b
Heloise Leblant <leblant@hotmail.fr>
2023-08-23 12:19:02 +0100

    form on show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def dashboard
     # @user = current_user
     # @bookings = Booking.where(user_id: current_user)
-    @bookings = Booking.where(vinyl_id: current_user.id)
+    @bookings = Booking.where(user_id: current_user.id)
     @vinyls = Vinyl.where(user_id: current_user.id)
   end
 end
