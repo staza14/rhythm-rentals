ea3acfd0cc104415b14da9bf8e9efbf09b7a85c8
Ollie Pugh <pughor@outlook.com>
2023-08-25 11:55:28 +0100

    Add dotenv-rails and added cards to landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @suggested_vinyls = Vinyl.all.sample(4)
   end
 
   def dashboard
